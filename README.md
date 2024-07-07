0298a7447d09019bf4bd27a33f1e23eb5eb8ddc0
Dev-Vasudevan <devvasudevan.wrk@gmail.com>
2024-07-07 13:51:53 +0530

    Created a vertex buffer

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # OpenGL
+
+GLEW Allows us to use modern opengl 
+Vertex buffeers is a way we send data to the gpu and then we can run a program called a
+vertex shader on the data in the gpu 
+Draw call will use the data and render yr object 
+All this is done in Modern Open gl 
+Open gl is a state machine 
+
+https://docs.gl/ This is very useful for opengl functions 
--- src/Application.cpp
+++ src/Application.cpp
@@ -24,6 +24,24 @@ int main(void)
     glfwMakeContextCurrent(window);
     glewInit();
     cout << glGetString(GL_VERSION) << endl;
+
+    unsigned int buffer1;
+    glGenBuffers(1, &buffer1);
+    //creates a buffer object with id buffer
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffer1);
+    //this binds the buffer object linked with unsigned int buffer to the array buffer (Array buffer is the target)
+    float points[6] = {
+        0.0f, -0.5,
+        1.0f, 0.5f,
+        0.0f, 0.5f
+    };
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points),points,GL_STATIC_DRAW);
+    //Here i copied all the data from the points array into the GLarrayBuffer which is bound to the unsigned int buffer ; 
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
