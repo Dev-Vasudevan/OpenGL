502f3f264455d9dd7d3127ff715cfc96015828ef
Dev-Vasudevan <devvasudevan.wrk@gmail.com>
2024-07-08 22:50:29 +0530

    Vertex Buffer Created

--- src/Application.cpp
+++ src/Application.cpp
@@ -32,14 +32,24 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer1);
     //this binds the buffer object linked with unsigned int buffer to the array buffer (Array buffer is the target)
     float points[6] = {
-        0.0f, -0.5,
-        1.0f, 0.5f,
+        0.0f, -0.5f,
+        -0.5f, -0.5f,
         0.0f, 0.5f
     };
     
     glBufferData(GL_ARRAY_BUFFER, sizeof(points),points,GL_STATIC_DRAW);
     //Here i copied all the data from the points array into the GLarrayBuffer which is bound to the unsigned int buffer ; 
 
+    glEnableVertexAttribArray(0);
+        //Allow
+
+    glVertexAttribPointer(0, 2, GL_FLOAT,GL_FALSE, sizeof(float) * 2, 0); 
+    // index 
+    // size = number of data items in each vertex attribute
+    // type =    Data type of each attribute
+    // If it has to be normalised or not 
+    // stride = size of each set of attributes ( x , y ) , texture ... aka vertex 
+    // pointer = size of each attribute 
 
 
     /* Loop until the user closes the window */
@@ -47,11 +57,8 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2d(0.0f, -0.5f);
-        glVertex2d(1.0f, 0.5f);
-        glVertex2d(0.0f, 0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+  
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
